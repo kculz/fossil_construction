55af0d51e9907ff78e5247735cbe9f9db19ba6a8
kculz <munyamakudzai095@gmail.com>
2023-09-16 15:05:09 +0200

    FIX: dashboard arrangement

--- app/src/layouts/Dashboard.jsx
+++ app/src/layouts/Dashboard.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const Dashboard = () => {
   return (
     <>
-      <div className="py-24">
-        <div className="grid grid-cols-3">
-          <aside className="flex flex-col gap-5 py-2 px-2 bg-green-600 h-full">
+      <div className="pt-24">
+        <div className="grid grid-cols-3 h-screen">
+          <aside className="flex flex-col items-center justify-center bg-green-600 shadow gap-10 font-semibold px-2 h-full">
             <Link>Dashboard</Link>
             <Link>My account</Link>
             <Link>Make request</Link>
@@ -14,7 +14,22 @@ const Dashboard = () => {
           </aside>
 
           <div className="col-span-2">
-            <h1 className="text-center">Dashboard</h1>
+            <h1 className="text-center py-10 text-2xl md:text-4xl font-semibold">Dashboard</h1>
+            <div className="flex justify-center items-center py-10 gap-5 md:px-10 px-10">
+              <div className="shadow rounded p-5 bg-white">
+                Make request
+              </div>
+
+              <div className="shadow rounded p-5 bg-white">
+                Chat to support
+              </div>
+
+              <div className="shadow rounded p-5 bg-white">
+                My Account
+              </div>
+
+             
+            </div>
           </div>
         </div>
       </div>
