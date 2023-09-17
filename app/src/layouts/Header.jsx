82c2a66e9aeefa15a6f56c4597d292e5889b89c9
kculz <munyamakudzai095@gmail.com>
2023-09-17 10:49:58 +0200

    FIX: header links

--- app/src/layouts/Header.jsx
+++ app/src/layouts/Header.jsx
@@ -11,22 +11,22 @@ const Header = () => {
     <>
     {/* Main Menu */}
         <header className="w-screen h-24 bg-gray-900/50 backdrop-blur fixed flex justify-between items-center lg:px-32 px-10 z-50">
-            <h1 className="text-xl lg:text-3xl text-white"><Link onClick={handleNav}>Fossil Construction</Link></h1>
+            <h1 className="text-xl lg:text-3xl text-white"><Link to="/" onClick={handleNav}>Fossil Construction</Link></h1>
             <ul className="md:flex hidden justify-center items-center gap-5 ">
                 <li>
-                    <Link className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">Home</Link>
+                    <Link to="/" className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">Home</Link>
                 </li>
                 <li>
                     <Link to="/services" className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">Services</Link>
                 </li>
                 <li>
-                    <Link className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">Contact Us</Link>
+                    <Link to="/contact" className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">Contact Us</Link>
                 </li>
                 <li>
                     <Link to="/request-for-quotation" className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">RFQ (Request for quotation)</Link>
                 </li>
                 <li>
-                    <Link className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">FAQ</Link>
+                    <Link to="/faq" className="lg:text-sm text-xs  text-gray-100 hover:text-green-900">FAQ</Link>
                 </li>
             </ul>
 
@@ -48,19 +48,19 @@ const Header = () => {
    {
     openNav &&  <ul className="w-full md:hidden fixed flex flex-col gap-5 top-24 bg-gray-900/50 backdrop-blur justify-center items-center px-10 py-10 z-50">
     <li>
-        <Link onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">Home</Link>
+        <Link to="/" onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">Home</Link>
     </li>
     <li>
         <Link to="/services" onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">Services</Link>
     </li>
     <li>
-        <Link onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">Contact Us</Link>
+        <Link to="/contact" onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">Contact Us</Link>
     </li>
     <li>
         <Link to="/request-for-quotation" onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">RFQ (Request for Quotation)</Link>
     </li>
     <li>
-        <Link onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">FAQ</Link>
+        <Link to="/faq" onClick={handleNav} className="text-gray-200 text-lg hover:text-green-900">FAQ</Link>
     </li>
 </ul>
    }
